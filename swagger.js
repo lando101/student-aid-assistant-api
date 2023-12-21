0d93a22373b44037cb19f2d2835bf83c2e822995
lando101 <landon.messmer2019@gmail.com>
2023-12-20 20:32:43 -0500

    initial commit

---
+++ swagger.js
@@ -0,0 +1,26 @@
+// swagger.js
+
+const swaggerJsdoc = require('swagger-jsdoc');
+const path = require('path');
+
+const options = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'OpenAI Express API',
+      version: '1.0.0',
+      description: 'API documentation for the OpenAI Express API',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000', // Update with your server URL
+        description: 'Local Development Server',
+      },
+    ],
+  },
+  apis: [path.resolve(__dirname, './app.js')], // Update with the path to your main Express application
+};
+
+const specs = swaggerJsdoc(options);
+
+module.exports = specs;
